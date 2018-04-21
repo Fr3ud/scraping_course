7d6f21f8bfcd9e0054195ffe8af4739b9e1cb076
Fr3ud <oo.pavelko@gmail.com>
2018-04-21 17:59:35 +0300

    2

--- 2/main.py
+++ 2/main.py
@@ -1,10 +1,24 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 
 def get_html(url):
     r = requests.get(url)
     return r.text
+
+def refined(str):
+    r = str.split(' ')[0]
+    result = r.replace(',', '')
+    return  result
+
+def write_csv(data):
+    with open('plugins.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        writer.writerow((data['name'],
+                        data['url'],
+                        data['reviews']))
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -14,16 +28,19 @@ def get_data(html):
     for plugin in plugins:
         name = plugin.find('h2').text
         url = plugin.find('h2').find('a').get('href')
-        rating = plugin.find('span', class_='rating-count').find('a').text
-        print(rating)
+        r = plugin.find('span', class_='rating-count').find('a').text
+        rating = refined(r)
 
-    # return plugins
+        data = {
+            'name': name,
+            'url': url,
+            'reviews': rating
+        }
+        write_csv(data)
 
 def main():
     url = 'https://wordpress.org/plugins'
     print(get_data(get_html(url)))
-
-
 
 if __name__ == '__main__':
     main()
